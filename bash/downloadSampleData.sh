63afd0e570198d0fd5d6e8b9cdc0f7f013e1aa71
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-11-30 12:10:55 -0500

    Adding input files donwload script and list of input files.

---
+++ bash/downloadSampleData.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+scp \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_sa/t2d_sa.sl_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_sa/t2d_sa.sp_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_sa/t2d_sa.ss_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_sa/t2d_sa.ss_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_ea/t2d_ea.eh_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_ea/t2d_ea.ek_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_ea/t2d_ea.ek_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_ea/t2d_ea.es_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_ea/t2d_ea.es_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_hs/t2d_hs.ha_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_hs/t2d_hs.hs_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_hs/t2d_hs.hs_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_hs/t2d_hs.sigma_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_hs/t2d_hs.sigma_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_aa/t2d_aa.aj_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_aa/t2d_aa.aw_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_aa/t2d_aa.aa_esp.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_aa/t2d_aa.ab_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_eu/t2d_eu.eu_esp.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_eu/t2d_eu.got2d.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_eu/t2d_eu.lucamp.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_eu/t2d_eu.ua_13k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_eu/t2d_eu.uf_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_eu/t2d_eu.ug_29k.ln_bmi_anal.vassoc \
+ silver:/home/unix/flannick/links/t2d_exomes/amp/55k/TM6SF2/t2d_eu/t2d_eu.um_13k.ln_bmi_anal.vassoc \
+ .
